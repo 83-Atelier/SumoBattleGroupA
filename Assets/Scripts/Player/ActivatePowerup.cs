bb8815a544043ae1a50644df314c840c7fce0138
Sammy-x3 <81697590+Sammy-x3@users.noreply.github.com>
2025-03-22 23:23:17 +0100

    feat: add power up ring functionality

--- Assets/Scripts/Player/ActivatePowerup.cs
+++ Assets/Scripts/Player/ActivatePowerup.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class ActivatePowerup : MonoBehaviour
 {
+    public GameObject powerUpRing;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RingPosition();
+    }
+
+    public void ActivateRing()
+    {
+        powerUpRing.SetActive(true);
+
+    }
+    public void DeactivateRing()
+    {
+        powerUpRing.SetActive(false);
+    }
+
+    private void RingPosition()
+    {
+        powerUpRing.transform.position = transform.position - new Vector3(0, 0.5f, 0);
+        powerUpRing.transform.Rotate(0, 100 * Time.deltaTime, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            ActivateRing();
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        powerUpRing.SetActive(false);
     }
 }
