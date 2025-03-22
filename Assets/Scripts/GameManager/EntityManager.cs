096e442e354f26ad84689d15546f4d9a6028a036
Mijo <matare.rod@gmail.com>
2025-03-22 16:08:07 -0500

    Enemy spawn logic and death.
    - Added Enemy prefab.
    - Added Enemy death logic.
    - Added Enemy wave spawns.
    - Made Follow_player script part of Enemy.
    
    * The EntityManager script is now part of the enemy prefab. This is not optimal.
    * FollowPlayer is not programmed yet.

--- Assets/Scripts/GameManager/EntityManager.cs
+++ Assets/Scripts/GameManager/EntityManager.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class EntityManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject enemyPrefab;
+    //public GameObject powerupPrefab;
+    private float _spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber;
     void Start()
     {
-        
+        SpawnEnemyWave(waveNumber);
     }
 
+    private Vector3 GenerateSpawnPos()
+    {
+        float spawnPosX = Random.Range(-_spawnRange, _spawnRange);
+        float spawnPosZ = Random.Range(-_spawnRange, _spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
     // Update is called once per frame
+
+    void SpawnEnemyWave(int numEnemies)
+    {
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPos(), enemyPrefab.transform.rotation);
+        }
+    }
     void Update()
     {
+        enemyCount = FindObjectsByType<FollowPlayer>(FindObjectsSortMode.None).Length;
+        if (enemyCount == 0)
+        {
+            //Instantiate(powerupPrefab, GenerateSpawnPos(), Quaternion.identity);
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+        }
         
+        if (transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
